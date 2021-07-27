631a8e0e92eff04dc67b43339a425db80c7476a8
한훈희 <hanjoi77@naver.com>
2021-07-27 17:35:48 +0900

    modify directory & add router

---
+++ frontend/src/views/user/NotFoundPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h1>Page is not found</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'NotFoundPage',
+}
+</script>
+
+<style>
+
+</style>
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    devServer: {
-        overlay: false
-    }
+  devServer: {
+    overlay: false,
+  },
+
+  transpileDependencies: ["vuetify"],
 };
