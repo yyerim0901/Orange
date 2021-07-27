a075c56c3de73532d9ad0a4a7a59e013fc3bd3c6
김재현 <h00335@naver.com>
2021-07-27 11:56:00 +0900

    write feed module

---
+++ mobile/features/ui-feed/src/main/java/com/keelim/features/ui/feed/FeedFragment.kt
@@ -0,0 +1,73 @@
+package com.keelim.features.ui.feed
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.keelim.features.ui.feed.databinding.FragmentFeedBinding
+import com.keelim.orange.common.toast
+import com.keelim.orange.data.model.Fight
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FeedFragment: Fragment() {
+    private var _binding: FragmentFeedBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel by viewModels<FeedViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFeedBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+        observeData()
+        viewModel.fetchData()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun initViews() = with(binding){
+        
+    }
+
+    private fun observeData() = viewModel.state.observe(viewLifecycleOwner) {
+        when (it) {
+            is FeedState.UnInitialized -> handleUnInitialized()
+            is FeedState.Loading -> handleLoading()
+            is FeedState.Success -> handleSuccess(it.data)
+            is FeedState.Error -> handleError()
+        }
+    }
+
+    private fun handleUnInitialized() {
+        requireActivity().toast("데이터 초기화 중입니다.")
+    }
+
+    private fun handleLoading() {
+        requireActivity().toast("데이터 초기화 중입니다.")
+    }
+
+    private fun handleSuccess(data: List<Fight>) {
+    }
+
+    private fun handleError() {
+        requireActivity().toast("에러가 발생했습니다. 다시 한번 로드해주세요")
+    }
+
+    companion object {
+        fun newInstance() = FeedFragment()
+    }
+}
---
+++ mobile/features/ui-feed/src/main/java/com/keelim/features/ui/feed/FeedViewModel.kt
@@ -0,0 +1,34 @@
+package com.keelim.features.ui.feed
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class FeedViewModel @Inject constructor(
+
+) : ViewModel() {
+    private val _state = MutableLiveData<FeedState>(FeedState.UnInitialized)
+    val state: LiveData<FeedState> get() = _state
+
+    fun fetchData() = viewModelScope.launch{
+        setState(
+            FeedState.Loading
+        )
+
+        setState(
+            FeedState.Success(
+                //todo()
+                emptyList()
+            )
+        )
+    }
+
+    private fun setState(value:FeedState){
+        _state.value = value
+    }
+}
