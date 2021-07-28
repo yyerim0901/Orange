a5799f0a3292c278e04033544aea531c650dff3b
김재현 <h00335@naver.com>
2021-07-28 15:18:26 +0900

    introduce android logger and fcm with push server

--- mobile/app/src/main/java/com/keelim/orange/MyApplication.kt
+++ mobile/app/src/main/java/com/keelim/orange/MyApplication.kt
@@ -1,20 +1,28 @@
 package com.keelim.orange
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.kakao.sdk.common.KakaoSdk
+import com.keelim.orange.data.repository.theme.ThemeRepository
 import com.keelim.orange.utils.ComponentLogger
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.launch
 
 
 @HiltAndroidApp
-class MyApplication: Application() {
-
+class MyApplication : Application() {
     @Inject
     lateinit var componentLogger: ComponentLogger
 
     override fun onCreate() {
         super.onCreate()
-        KakaoSdk.init(this, TODO())
+//        KakaoSdk.init(this, TODO())
+        componentLogger.initialize(this)
+
     }
 }
