c919cb27a7a2dab51893813572fce76094a18678
한훈희 <hanjoi77@naver.com>
2021-07-30 18:06:04 +0900

    modify router, loginForm, signupForm

--- frontend/src/components/user/LoginForm.vue
+++ frontend/src/components/user/LoginForm.vue
@@ -1,15 +1,42 @@
 <template>
-  <form @submit.prevent="submitForm">
-    <div>
-      <label for="email">id: </label>
-      <input id="email" type="text" v-model="email">
-    </div>
-    <div>
-      <label for="password">pw: </label>
-      <input id="password" type="text" v-model="password">
-    </div>
-    <button type="submit">로그인</button>
-  </form>
+  <v-form ref="form" lazy-validation>
+    <v-row class="container">
+      <v-col cols="12">
+        <v-text-field
+        v-model="email"
+        id="email"
+        label="email"
+        :rules="emailRules"
+        required
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="password"
+          id="password"
+          :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+          :rules="passwordRules"
+          type="password"
+          label="password"
+          hint="At least 8 characters"
+          counter
+          @click:append="show1 = !show1"
+        ></v-text-field>
+      </v-col>
+
+      <v-checkbox
+        v-model="checkbox"
+        id="checkbox"
+        :rules="[v => !!v || 'You must agree to continue!']"
+        label="약관에 동의하시겠습니까?"
+        required
+      ></v-checkbox>
+
+      <v-btn class="mr-4" @click="submitForm">
+        로그인
+      </v-btn>
+    </v-row>
+  </v-form>
 </template>
 
 <script>
@@ -17,18 +44,36 @@ export default {
   name: 'LoginForm',
   data() {
     return {
+      state:'ins',
       email: '',
+      emailRules: [
+        v => !!v || '이메일은 필수 입력사항입니다.',
+        v => /.+@.+\..+/.test(v) || '해당 이메일을 사용할 수 없습니다.',
+      ],
+      passwordRules: [
+        v => this.state === 'ins' ? !!v || '패스워드는 필수 입력사항입니다.' : true,
+        v => !(v && v.length >= 20) || '패스워드는 20자 이상 입력할 수 없습니다.',
+        v => v.length >= 8 || 'At least 8 characters',
+      ],
+      show1: false,
       password: '',
+      checkbox: false,
     }
   },
+  
   methods: {
     submitForm() {
       console.log("로그인 완료")
-    }
+    },
+    validate () {
+      this.$refs.form.validate()
+    },
   }
 }
 </script>
 
 <style>
-
+.btn-color {
+  color: #FF9800;
+}
 </style>
