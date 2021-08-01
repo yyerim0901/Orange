e8e76c1c3e7d3a96416f16852e933ed514112627
김재현 <h00335@naver.com>
2021-08-01 22:02:03 +0900

    write data model Favorite

---
+++ mobile/data/src/main/java/com/keelim/orange/data/model/Favorite.kt
@@ -0,0 +1,9 @@
+package com.keelim.orange.data.model
+
+data class Favorite(
+    val imageLink: String,
+    val title: String,
+    val level: String,
+    val number: String,
+    val rating: Float,
+)
