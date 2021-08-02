74a0700ac6d4cba365426280e89fba9709b92eb6
한훈희 <hanjoi77@naver.com>
2021-08-02 13:25:48 +0900

    add vee-validate, modify login

--- frontend/src/components/user/LoginForm.vue
+++ frontend/src/components/user/LoginForm.vue
@@ -1,69 +1,128 @@
 <template>
-  <v-form ref="form" lazy-validation @submit.prevent="submitForm">
-    <v-row class="container">
-      <v-col cols="12">
-        <v-text-field
-        v-model="email"
-        id="email"
-        label="email"
-        :rules="emailRules"
-        required
-        ></v-text-field>
+  <v-container class="fill-height">
+    <v-row justify="center">
+      <v-col cols="auto">
+        <v-card
+          width="460"
+        >
+          <v-card-text class="text-center px-12 py-16">
+            <validation-observer
+              ref="observer"
+              v-slot="{ invalid }"
+            >
+              <v-form
+                ref="form"
+                @submit.prevent="signIn"
+              >
+                <div class="text-h4 font-weight-black mb-10">
+                  로그인
+                </div>
+                <validation-provider
+                  v-slot="{ errors }"
+                  name="이메일"
+                  :rules="{
+                    required: true,
+                  }"
+                >
+                  <v-text-field
+                    v-model="email"
+                    label="이메일"
+                    clearable
+                    prepend-icon="mdi-email"
+                    :error-messages="errors"
+                  />
+                </validation-provider>
+                <validation-provider
+                  v-slot="{ errors }"
+                  name="비밀번호"
+                  :rules="{
+                    required: true,
+                  }"
+                >
+                  <v-text-field
+                    v-model="password"
+                    label="비밀번호"
+                    clearable
+                    prepend-icon="mdi-lock-outline"
+                    :error-messages="errors"
+                  />
+                </validation-provider>
+                <v-btn
+                  class="mt-6"
+                  type="submit"
+                  block
+                  x-large
+                  rounded
+                  color="primary"
+                  :disabled="invalid"
+                >
+                  로그인
+                </v-btn>
+                <div class="mt-5">
+                  <router-link
+                    class="text-decoration-none"
+                    to="/"
+                  >
+                    홈
+                  </router-link> |
+                  <router-link
+                    class="text-decoration-none"
+                    to="/signup"
+                  >
+                    회원가입
+                  </router-link>
+                </div>
+              </v-form>
+            </validation-observer>
+          </v-card-text>
+        </v-card>
       </v-col>
-      <v-col cols="12">
-        <v-text-field
-          v-model="password"
-          id="password"
-          :rules="passwordRules"
-          type="password"
-          label="password"
-        ></v-text-field>
-      </v-col>
-
-      <v-checkbox
-        v-model="checkbox"
-        id="checkbox"
-        :rules="[v => !!v || 'You must agree to continue!']"
-        label="약관에 동의하시겠습니까?"
-        required
-      ></v-checkbox>
-
-      <v-btn class="mr-4" @click="submitForm">
-        로그인
-      </v-btn>
     </v-row>
-  </v-form>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: 'LoginForm',
-  data() {
-    return {
-      state:'ins',
-      email: '',
-      emailRules: [
-        v => !!v || '이메일은 필수 입력사항입니다.',
-        v => /.+@.+\..+/.test(v) || '해당 이메일을 사용할 수 없습니다.',
-      ],
-      passwordRules: [
-        v => this.state === 'ins' ? !!v || '패스워드는 필수 입력사항입니다.' : true,
-        v => !(v && v.length >= 20) || '패스워드는 20자 이상 입력할 수 없습니다.',
-        v => v.length >= 8 || 'At least 8 characters',
-      ],
-      password: '',
-      checkbox: false,
-    }
-  },
-  
+    data: () => ({
+    email: null,
+    password: null,
+  }),
   methods: {
-    submitForm() {
-      console.log("로그인 완료")
-    },
-    validate () {
-      this.$refs.form.validate()
-    },
+    signIn () {
+      this.$refs.observer.validate().then(result => {
+        if (result) {
+          alert('로그인 성공')
+        }
+      })
+    }
   }
+  // data() {
+  //   return {
+  //     state:'ins',
+  //     email: '',
+  //     emailRules: [
+  //       v => !!v || '이메일은 필수 입력사항입니다.',
+  //       v => /.+@.+\..+/.test(v) || '해당 이메일을 사용할 수 없습니다.',
+  //     ],
+  //     passwordRules: [
+  //       v => this.state === 'ins' ? !!v || '패스워드는 필수 입력사항입니다.' : true,
+  //       v => !(v && v.length >= 20) || '패스워드는 20자 이상 입력할 수 없습니다.',
+  //       v => v.length >= 8 || 'At least 8 characters',
+  //     ],
+  //     password: '',
+  //     checkbox: false,
+  //   }
+  // },
+  
+  // methods: {
+  //   submitForm() {
+  //     console.log("로그인 완료")
+  //   },
+  //   validate () {
+  //     this.$refs.form.validate()
+  //   },
+  // }
 }
 </script>
 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import './plugins';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
