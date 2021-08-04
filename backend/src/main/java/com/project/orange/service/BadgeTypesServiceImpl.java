8083523e9377f9c7c37a85187fb33db0668e7424
임성준 <jundlatjdwns@naver.com>
2021-08-04 10:20:29 +0900

    Settings Badge-controller

---
+++ backend/src/main/java/com/project/orange/service/BadgeTypesServiceImpl.java
@@ -0,0 +1,24 @@
+package com.project.orange.service;
+
+import com.project.orange.entity.badge.BadgeTypes;
+import com.project.orange.repository.badge.BadgeTypesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class BadgeTypesServiceImpl implements BadgeTypesService {
+
+    @Autowired
+    private BadgeTypesRepository badgeTypesRepository;
+
+    @Override
+    public Optional<BadgeTypes> selectOne(Long badgeTypesId) { return badgeTypesRepository.findById(badgeTypesId); }
+
+    @Override
+    public List<BadgeTypes> selectAll() { return badgeTypesRepository.findAll(); }
+}
