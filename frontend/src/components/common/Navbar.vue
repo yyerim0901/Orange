3928d7ed90b649c68f549e90b46caf0bad14f322
한훈희 <hanjoi77@naver.com>
2021-08-04 16:55:28 +0900

    notification ver1

--- frontend/src/components/common/Navbar.vue
+++ frontend/src/components/common/Navbar.vue
@@ -2,33 +2,51 @@
   <div>
     <b-navbar variant="navbar-light bg-light justify-content-between" type="light">
       <b-navbar-brand tag="h1">
-        <router-link to="/" class="nav-brand-text">Orange</router-link>
+        <v-btn 
+          text
+          color="orange"
+          @click="goHome"
+        >
+        Orange</v-btn>
       </b-navbar-brand>
       <b-navbar-nav class="d-flex">
-        <b-nav-item>About</b-nav-item>
-        <b-nav-item><b-icon icon="bell"></b-icon></b-nav-item>
+        <b-nav-item>
+          <v-btn text> About </v-btn></b-nav-item>
         
-        <b-nav-item><router-link to="/profile"><b-icon icon="person"></b-icon></router-link></b-nav-item>
+        <b-nav-item><NotificationForm /></b-nav-item>
         
+        <b-nav-item>
+          <v-btn 
+            icon
+            @click="goProfile"
+          >
+            <v-icon>mdi-account</v-icon>
+          </v-btn>
+        </b-nav-item>
       </b-navbar-nav>
     </b-navbar>
   </div>
 </template>
 
 <script>
-import AppHeader from '@/components/common/AppHeader.vue'
+import NotificationForm from '@/components/main/NotificationForm.vue'
 
 export default {
   name: 'Navbar',
   components: {
-    AppHeader,
+    NotificationForm,
+  },
+  methods: {
+    goProfile() {
+      this.$router.push({path:'/profile'}).catch(()=> {});
+    },
+    goHome() {
+      this.$router.push({path:'/'}).catch(()=> {});
+    },
   }
 }
 </script>
 
 <style>
-.nav-brand-text {
-  text-decoration: none;
-  color: black;
-}
+
 </style>
