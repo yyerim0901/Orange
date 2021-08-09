521084ac06db10b64239896416b09b7922c73ab4
김재현 <h00335@naver.com>
2021-08-09 13:11:04 +0900

    write database module

---
+++ mobile/data/src/main/java/com/keelim/orange/data/model/History.kt
@@ -0,0 +1,11 @@
+package com.keelim.orange.data.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class History(
+    @PrimaryKey val uid: Int?,
+    @ColumnInfo(name = "keyword") val keyword: String?
+)
