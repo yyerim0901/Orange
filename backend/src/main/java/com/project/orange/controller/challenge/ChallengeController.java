82392e34bfcf4deb6171e936e44e045b21b91407
배세영 <triplet02@naver.com>
2021-08-11 14:17:19 +0900

    [backend] no plain text response

--- backend/src/main/java/com/project/orange/controller/challenge/ChallengeController.java
+++ backend/src/main/java/com/project/orange/controller/challenge/ChallengeController.java
@@ -5,14 +5,19 @@ import com.project.orange.entity.challenge.Challenges;
 import com.project.orange.entity.notification.Notifications;
 import com.project.orange.entity.user.UsersChallenges;
 import com.project.orange.service.challenge.ChallengeService;
+import com.project.orange.service.nofitication.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+
+import static com.project.orange.management.Constants.challengeNewMemberRegisteredContent;
+import static com.project.orange.management.Constants.challengeNewMemberRegisteredTitle;
 
 @RestController
 @RequestMapping("/api/challenge")
@@ -20,6 +25,9 @@ public class ChallengeController {
 
     @Autowired
     private ChallengeService challengeService;
+
+    @Autowired
+    private NotificationService notificationService;
 
     @GetMapping("/{challengeId}")
     public ResponseEntity<Challenges> selectOne(@PathVariable Long challengeId){
@@ -45,7 +53,7 @@ public class ChallengeController {
     }
 
     @GetMapping("/ranking/point")
-    public ResponseEntity<List<Challenges>> selectAllSoretedByTotalPointDesc(){
+    public ResponseEntity<List<Challenges>> selectAllSortedByTotalPointDesc(){
         List<Challenges> list;
         list = challengeService.selectAllSortedByTotalPoint();
         if (list == null || list.isEmpty()) {
@@ -78,53 +86,74 @@ public class ChallengeController {
     }
 
     @PostMapping("/register/new-user")
-    public ResponseEntity<String>  registerNewUserToChallenge(@RequestBody Map<String, Long> parameters){
+    public ResponseEntity<Map<String, String>>  registerNewUserToChallenge(@RequestBody Map<String, Long> parameters){
         Long challengeId = parameters.get("challengeId");
         Long userId = parameters.get("userId");
         Optional<Challenges> targetChallenge = challengeService.selectByChallengeId(challengeId);
+        Map<String, String> response = new HashMap<>();
         if(targetChallenge.isEmpty()){
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
 
         if(challengeService.isUserAlreadyInChallenge(challengeId, userId)){
-            return new ResponseEntity<>("Already In Challenge",HttpStatus.OK);
+            response.put("result", "Already In Challenge");
+            return new ResponseEntity<>(response, HttpStatus.OK);
         } else {
             Optional<UsersChallenges> newUser = challengeService.registerNewUserToChallenge(challengeId, userId);
             if(newUser.isEmpty()){
-                return new ResponseEntity<>("Failed", HttpStatus.NO_CONTENT);
+                response.put("result", "Failed");
+                return new ResponseEntity<>(response, HttpStatus.NO_CONTENT);
             } else {
                 Long managerId = targetChallenge.get().getManagerId();
-                return new ResponseEntity<>("Success", HttpStatus.OK);
+                Optional<Notifications> notification;
+                notification = notificationService.sendNotificationTo(
+                        managerId,
+                        challengeNewMemberRegisteredTitle,
+                        challengeNewMemberRegisteredContent
+                );
+                if(notification.isPresent()) {
+                    response.put("result", "Success");
+                    return new ResponseEntity<>(response, HttpStatus.OK);
+                } else {
+                    response.put("result", "Notification Error");
+                    return new ResponseEntity<>(response, HttpStatus.NO_CONTENT);
+                }
             }
         }
     }
 
     @PostMapping("/register/new-challenge")
-    public ResponseEntity<String> registerNewChallenge(@RequestBody Challenges challenge){
+    public ResponseEntity<Map<String, String>> registerNewChallenge(@RequestBody Challenges challenge){
         Optional<BattleMatching> matchMakingResult;
         matchMakingResult = challengeService.registerNewChallenge(challenge);
-
+        Map<String, String> response = new HashMap<>();
         if(matchMakingResult.isPresent()){
             // Todo : notification 연동
             List<Notifications> notificationsList = challengeService.notifyMatchMaking(matchMakingResult.get());
             if(notificationsList != null && !notificationsList.isEmpty()) {
-                return new ResponseEntity<>("matched", HttpStatus.OK);
+                response.put("result", "matched");
+                return new ResponseEntity<>(response, HttpStatus.OK);
             } else {
-                return new ResponseEntity<>("Notification Error", HttpStatus.NO_CONTENT);
+                response.put("result", "Notification Error");
+                return new ResponseEntity<>(response, HttpStatus.NO_CONTENT);
             }
         }
         else {
-            return new ResponseEntity<>("unmatched", HttpStatus.OK);
+            response.put("result", "unmatched");
+            return new ResponseEntity<>(response, HttpStatus.OK);
         }
     }
 
     @PutMapping("/update")
-    public ResponseEntity<String> modify(@RequestBody Challenges challenge){
+    public ResponseEntity<Map<String, String>> modify(@RequestBody Challenges challenge){
         Optional<Challenges> modifiedChallenge = challengeService.save(challenge);
+        Map<String, String> response = new HashMap<>();
         if(modifiedChallenge.isEmpty()){
-            return new ResponseEntity<>("failed", HttpStatus.NO_CONTENT);
+            response.put("result", "failed");
+            return new ResponseEntity<>(response, HttpStatus.NO_CONTENT);
         } else {
-            return new ResponseEntity<>("success", HttpStatus.OK);
+            response.put("result", "success");
+            return new ResponseEntity<>(response, HttpStatus.OK);
         }
     }
 }
--- backend/src/main/java/com/project/orange/service/nofitication/NotificationService.java
+++ backend/src/main/java/com/project/orange/service/nofitication/NotificationService.java
@@ -4,8 +4,10 @@ import com.project.orange.entity.notification.Notifications;
 import com.project.orange.entity.user.Users;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface NotificationService {
     public List<Notifications> selectAllByUserId(Long userId);
     public void deleteByNotificationId(Long notificationId);
+    public Optional<Notifications> sendNotificationTo(Long userId, String title, String content);
 }
