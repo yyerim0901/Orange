965f922946bb83b90e0b9b9e753027f938e8c90b
배세영 <triplet02@naver.com>
2021-08-12 15:06:13 +0900

    [backend] badge awarding method

--- backend/src/main/java/com/project/orange/service/user/BadgesUsersServiceImpl.java
+++ backend/src/main/java/com/project/orange/service/user/BadgesUsersServiceImpl.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import static com.project.orange.management.Constants.HandsInHandsBadgeId;
+import static com.project.orange.management.Constants.HereComesANewChallengerBadgeId;
 
 @Service
 @Transactional
@@ -19,5 +23,23 @@ public class BadgesUsersServiceImpl implements BadgesUsersService{
     @Override
     public List<BadgesUsers> selectAllByUserId(Long user) {
         return badgesUsersRepository.findAllByUser(user);
+    }
+
+    @Override
+    public Optional<BadgesUsers> badgeAward(Long userId, Long badgeId) {
+        List<BadgesUsers> firstTimeChallengeJoin = new ArrayList<>();
+        firstTimeChallengeJoin = badgesUsersRepository.findByUserAndBadge(userId, badgeId);
+
+        if(firstTimeChallengeJoin.isEmpty()) {
+            BadgesUsers handsInHandsBadge = BadgesUsers.builder()
+                    .badge(badgeId)
+                    .user(userId)
+                    .badgeCount(1)
+                    .build();
+
+            return Optional.of(badgesUsersRepository.save(handsInHandsBadge));
+        } else {
+            return Optional.empty();
+        }
     }
 }
