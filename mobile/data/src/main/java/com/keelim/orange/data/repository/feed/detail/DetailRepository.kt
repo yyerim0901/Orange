a31be64922cd3f725943b7b598eea9f2b36317be
김재현 <h00335@naver.com>
2021-08-13 01:34:27 +0900

    delete detailRepository

--- mobile/data/src/main/java/com/keelim/orange/data/repository/feed/detail/DetailRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.keelim.orange.data.repository.feed.detail
-
-import com.keelim.orange.data.response.DetailResponse
-
-interface DetailRepository {
-  suspend fun getDetail(uid: String): DetailResponse
-}
