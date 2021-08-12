127c430ece855fb4dc6cc392abf3663d07d19e79
한훈희 <hanjoi77@naver.com>
2021-08-12 18:48:58 +0900

    token ver1

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,8 +1,13 @@
-import axios from 'axios'
+import axios from 'axios';
+import { setInterceptors } from './common/interceptors';
 
-const instance = axios.create({
-  baseURL: 'http://i5b102.p.ssafy.io:8181'
-})
+function createInstance() {
+  const instance = axios.create({
+    baseURL: 'http://i5b102.p.ssafy.io:8181',
+  })
+  return setInterceptors(instance);
+}
+const instance = createInstance()
 
 function registerUser(userData) {
   return instance.post('/api/user/signup', userData)
@@ -11,7 +16,6 @@ function registerUser(userData) {
 function loginUser(userData) {
   return instance.post('/api/user/login', userData)
 }
-
 
 
 export { registerUser, loginUser };
