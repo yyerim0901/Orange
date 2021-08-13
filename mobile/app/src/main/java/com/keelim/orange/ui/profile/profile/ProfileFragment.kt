443d629c038468053b42de68987dda82566b39dd
김재현 <h00335@naver.com>
2021-08-13 14:32:06 +0900

    move profile code

--- mobile/app/src/main/java/com/keelim/orange/ui/profile/profile/ProfileFragment.kt
+++ mobile/app/src/main/java/com/keelim/orange/ui/profile/profile/ProfileFragment.kt
@@ -1,4 +1,4 @@
-package com.keelim.orange.ui.profile
+package com.keelim.orange.ui.profile.profile
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,7 +8,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.keelim.orange.R
-import com.keelim.orange.common.toast
 import com.keelim.orange.databinding.FragmentProfileBinding
 import dagger.hilt.android.AndroidEntryPoint
 
