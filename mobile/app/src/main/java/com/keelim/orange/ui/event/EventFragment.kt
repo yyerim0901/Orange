4b9ad8b513e0b361a9dadef238f81fe694b21b41
김재현 <h00335@naver.com>
2021-08-16 22:38:44 +0900

    write event fragment

---
+++ mobile/app/src/main/java/com/keelim/orange/ui/event/EventFragment.kt
@@ -0,0 +1,50 @@
+package com.keelim.orange.ui.event
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.keelim.orange.R
+import com.keelim.orange.databinding.FragmentEventBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class EventFragment:Fragment() {
+    private var _binding:FragmentEventBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentEventBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun initViews() = with(binding){
+        val anim = AnimationUtils.loadAnimation(requireContext(), R.anim.zoom_in)
+        lifecycleScope.launch {
+            delay(3000)
+            second.startAnimation(anim)
+            first.startAnimation(anim)
+        }
+    }
+}
--- mobile/app/src/main/java/com/keelim/orange/ui/feed/FeedFragment.kt
+++ mobile/app/src/main/java/com/keelim/orange/ui/feed/FeedFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -145,6 +144,9 @@ class FeedFragment : Fragment() {
                 imageView.load(images[position])
             }
             show()
+        }
+        carouselView.setOnClickListener {
+            findNavController().navigate(R.id.eventFragment)
         }
     }
 
