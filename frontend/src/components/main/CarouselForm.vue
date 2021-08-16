df5e8426b1a007e9929d2253ab7723c21e588efd
한훈희 <hanjoi77@naver.com>
2021-08-16 21:44:22 +0900

    modify carousel

--- frontend/src/components/main/CarouselForm.vue
+++ frontend/src/components/main/CarouselForm.vue
@@ -1,51 +1,64 @@
 <template>
   <v-carousel
     cycle
-    height="400"
+    height="auto"
     hide-delimiter-background
     show-arrows-on-hover
   >
     <v-carousel-item
-      v-for="(slide, i) in slides"
+      v-for="(item, i) in items"
       :key="i"
+      :src="item.src"
     >
-      <v-sheet
-        :color="colors[i]"
-        height="100%"
-      >
+      <v-sheet>
         <v-row
           class="fill-height"
           align="center"
           justify="center"
         >
-          <div class="text-h2">
-            {{ slide }} Slide
-          </div>
         </v-row>
       </v-sheet>
+      <template v-slot:placeholder>
+        <v-row
+          class="fill-height ma-0"
+          align="center"
+          justify="center"
+        >
+          <v-progress-circular
+            indeterminate
+            color="grey lighten-5"
+          ></v-progress-circular>
+        </v-row>
+      </template>
     </v-carousel-item>
   </v-carousel>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        colors: [
-          'indigo',
-          'warning',
-          'pink darken-2',
-          'red lighten-1',
-          'deep-purple accent-4',
-        ],
-        slides: [
-          'First',
-          'Second',
-          'Third',
-          'Fourth',
-          'Fifth',
-        ],
-      }
-    },
-  }
+import carousel1 from '@/assets/images/carousel1.jpg'
+import carousel3 from '@/assets/images/carousel3.jpg'
+import carousel6 from '@/assets/images/carousel6.jpg'
+export default {
+  data () {
+    return {
+       items: [
+         {
+           src: carousel1,
+         },
+         {
+           src: carousel3,
+         },
+         {
+           src: carousel6,
+         },
+       ]
+    }
+  },
+}
 </script>
+<style>
+.carousel_img {
+  width: 100%;
+  max-height: 100%;
+}
+</style>
