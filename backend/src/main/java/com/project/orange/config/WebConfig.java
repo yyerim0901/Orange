0395bfded6a945c30c57b172f3ab550a9ec770ad
배세영 <triplet02@naver.com>
2021-08-17 13:51:14 +0900

    [backend] saved image into url response api

--- backend/src/main/java/com/project/orange/config/WebConfig.java
+++ backend/src/main/java/com/project/orange/config/WebConfig.java
@@ -2,7 +2,11 @@ package com.project.orange.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static com.project.orange.management.Constants.*;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -11,5 +15,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler(ImageUploadUrl)
+                .addResourceLocations(ResourcePath);
     }
 }
